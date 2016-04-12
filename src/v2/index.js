92a2eba30a320f843749c0a79697d1df5d815e8c
Brian <brian@addictivemobility.com>
2016-04-12 09:49:02 -0400

    added support for validating array of scalar values

--- src/v2/index.js
+++ src/v2/index.js
@@ -4,38 +4,75 @@ var reduce = require('lodash.reduce');
 
 function validate (source, schema, fieldPrefix) {
     var _fieldPrefix = (fieldPrefix + '.') || '';
-    
-    if (Array.isArray(source)) {
-        return reduce(source, function (errors, item, index) {
-            var itemErrors = validate(item, schema, index);
-            return errors.concat(itemErrors);
-        }, []);   
+
+
+    if (typeof schema === 'function') {
+        return processScalar(source, schema);
     }
-    
+
+    if (Array.isArray(source)) {
+        return processNested(source, schema);
+    }
+
+    return processTop(source, schema, _fieldPrefix)
+}
+
+function processScalar (source, schema) {
+    return reduce(source, function (errors, item, index) {
+
+        var result = schema(item);
+
+        if (typeof result === 'string') {
+            return errors.concat({
+                field: index,
+                value: item,
+                message: result
+            });
+        }
+
+        return errors;
+    }, []);
+}
+
+function processNested (source, schema) {
+    return reduce(source, function (errors, item, index) {
+        var itemErrors = validate(item, schema, index);
+        return errors.concat(itemErrors);
+    }, []);
+}
+
+function processTop (source, schema, fieldPrefix) {
     return reduce(schema, function (errors, validator, fieldName) {
 
         var valueToValidate = source[fieldName];
 
         var result = validator(valueToValidate, source);
 
+
+        // result is an array when doing nested validation
         if (Array.isArray(result)) {
-            return errors.concat(result);
+
+            // prefix child error field with the parent's field name
+            return result.map(function (error) {
+                return Object.assign({}, error, {
+                    field: fieldName + '.' + error.field
+                });
+            })
+            .concat(errors);
         }
 
         if (typeof result === 'string') {
             return errors.concat({
-                field: _fieldPrefix + fieldName,
+                field: fieldPrefix + fieldName,
                 value: valueToValidate,
                 message: result
             });
         }
-    
+
         return errors;
 
     }, []);
 }
-
-
 
 
 /*
@@ -62,8 +99,8 @@ var data = {
         { name: 'brian' },
         123
     ],
-    
-    
+
+
 }
 
 var personValidator = {
@@ -87,35 +124,35 @@ var fruitValidator = {
 // friends.4.address.1.street
 
 var validators = {
-    
+
     foo (value) {
         if (!isNumber(value)) {
             return 'must be an array'
         }
     }
     friends (value) {
-        
+
         if (!isArray(value)) {
             return 'must be an array'
         }
-        
-        return ezv(thing, schema) // thing as array 
+
+        return ezv(thing, schema) // thing as array
     }
-    
+
     friends (value) {
         return ezv(thing, schema) // thing as object
     }
-    
+
     friends: [
         function () {
-            
-        } 
+
+        }
     ],
     bar: [
         function (value) {
-            
+
             return ezv(value, otherschema) // array
-            
+
             return {
                 pass: false,
                 message:
@@ -130,7 +167,7 @@ var validators = {
         },
         {
             validate () {
-                
+
             }
         }
     ],
--- tests/v2.js
+++ tests/v2.js
@@ -3,148 +3,127 @@ var isPlainObject = require('lodash.isplainobject');
 var ezv2 = require('../src/v2');
 
 describe('ezv2', function () {
-    
-    describe('scalar properties', function () {
+
+    describe('object', function () {
+        var schema = {
+            age: function (value) {
+                if (typeof value !== 'number') {
+                    return 'age must be a number'
+                }
+            }
+        }
+
         it('should pass when a valid number', function () {
-        
+
             var input = {
                 age: 123
             };
-            
-            var schema = {
-                age: function (value) {
-                    if (typeof value !== 'number') {
-                        return 'age must be a number'
-                    }
-                }
-            }
-            
+
             errors = ezv2(input, schema);
-            
+
             assert(errors.length === 0);
         });
-        
+
         it('should fail when provided an invalid number', function () {
-        
+
             var input = {
                 age: '123'
             };
-            
-            var schema = {
-                age: function (value) {
-                    if (typeof value !== 'number') {
-                        return 'age must be a number'
-                    }
-                }
-            }
-            
+
             errors = ezv2(input, schema);
-            
+
             assert(errors.length === 1);
         });
     });
-    
+
     describe('nested objects', function () {
-        
+
+        var addressSchema = {
+            street: function (value) {
+                if (typeof value !== 'string') {
+                    return 'street must be a string'
+                }
+            }
+        }
+
+        var contactSchema = {
+            address: function (value) {
+                if (!isPlainObject(value)) {
+                    return 'address must be an object'
+                }
+
+                return ezv2(value, addressSchema);
+            }
+        }
+
         it('should pass when street is a string', function () {
-        
+
             var input = {
                 address: {
                     street: '12 fake st'
                 }
             };
-            
-            var addressSchema = {
-                street: function (value) {
-                    if (typeof value !== 'string') {
-                        return 'street must be a string'
-                    }
-                }
-            }
-            
-            var schema = {
-                address: function (value) {
-                    if (!isPlainObject(value)) {
-                        return 'address must be an object'
-                    }
-                    
-                    return ezv2(value, addressSchema);
-                }
-            }
-            
-            errors = ezv2(input, schema);
-            
+
+
+            errors = ezv2(input, contactSchema);
+
             assert(errors.length === 0);
         });
-        
+
         it('should fail when street is a number', function () {
-        
+
             var input = {
                 address: {
                     street: 123
                 }
             };
-            
-            var addressSchema = {
-                street: function (value) {
-                    if (typeof value !== 'string') {
-                        return 'street must be a string'
-                    }
-                }
-            }
-            
-            var schema = {
-                address: function (value) {
-                    if (!isPlainObject(value)) {
-                        return 'address must be an object'
-                    }
-                    
-                    return ezv2(value, addressSchema);
-                }
-            }
-            
-            errors = ezv2(input, schema);
-            
+
+            errors = ezv2(input, contactSchema);
+
             assert(errors.length === 1);
         });
-        
-        
-        it('should handle arrays of objects', function () {
+
+    });
+
+    describe('nested array of objects', function () {
+
+        var personSchema = {
+            friends: function (value) {
+                return ezv2(value, friendSchema);
+            }
+        }
+
+        var friendSchema = {
+            name: function (value) {
+                if (typeof value !== 'string') {
+                    return 'name must be a string'
+                }
+            }
+        }
+
+        it('should fail when name is not a string', function () {
+
             var input = {
                 friends: [
                     { name: 'brian' },
                     { name: 123 },
                     { name: false }
-                ]   
+                ]
             }
-            var personSchema = {
-                friends: function (value) {
-                    return ezv2(value, friendSchema);
-                }
-            }
-            
-            var friendSchema = {
-                name: function (value) {
-                    if (typeof value !== 'string') {
-                        return 'name must be a string'
-                    }
-                }
-            }
-            
-            errors = ezv2(input, personSchema);
-            console.log(errors)
-            assert(errors.length === 2);
-       }) 
-    });
 
-    describe('arrays', function () {
-       it('should handle arrays of objects', function () {
-           var input = [
-               { name: 'brian' },
-               { name: 123 },
-               { name: false },
-           ]
-            
+            errors = ezv2(input, personSchema);
+            assert(errors.length === 2);
+        })
+    })
+
+    describe('array of objects', function () {
+        it('should fail when name is not a string', function () {
+            var input = [
+                { name: 'brian' },
+                { name: 123 },
+                { name: true },
+            ]
+
             var schema = {
                 name: function (value) {
                     if (typeof value !== 'string') {
@@ -152,9 +131,25 @@ describe('ezv2', function () {
                     }
                 }
             }
-            
+
             errors = ezv2(input, schema);
             assert(errors.length === 2);
-       }) 
+        });
     });
+
+    describe('array of scalar', function () {
+
+        function schema (value) {
+            if (typeof value !== 'string') {
+                return 'must be a string'
+            }
+        }
+
+        it('should fail when provided non-strings', function () {
+            var input = [1, 2, 'a', 'b'];
+
+            errors = ezv2(input, schema);
+            assert(errors.length === 2);
+        });
+    })
 });
